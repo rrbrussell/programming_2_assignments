6e9d56b778772945b2037725509e35242ef28982
Robert R. Russell <robert@rrbrussell.com>
2011-01-28 12:54:58 -0600

    Changed some variable name to be more descriptive

--- assignment_2/common_datatypes.h
+++ assignment_2/common_datatypes.h
@@ -1,12 +1,12 @@
 /* programming_2/assignment_2/common_datatypes.h
-   Assignment 2 commont datatypes
+   Assignment 2 common datatypes
 */
 
 #IFNDEF ASSIGNMENT_2_COMMON_DATATYPES_H
 #DEFINE ASSIGNMENT_2_COMMON_DATATYPES_H
 struct bounds_struct {
-  int upper;
-  int lower;
+  int starting;
+  int ending;
   int increment;
 };
 
