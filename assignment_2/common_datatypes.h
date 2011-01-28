a4446f70020b6b964783c680ec9656974adb885c
Robert R. Russell <robert@rrbrussell.com>
2011-01-28 12:11:47 -0600

    Added a structure for the bounds

---
+++ assignment_2/common_datatypes.h
@@ -0,0 +1,13 @@
+/* programming_2/assignment_2/common_datatypes.h
+   Assignment 2 commont datatypes
+*/
+
+#IFNDEF ASSIGNMENT_2_COMMON_DATATYPES_H
+#DEFINE ASSIGNMENT_2_COMMON_DATATYPES_H
+struct bounds_struct {
+  int upper;
+  int lower;
+  int increment;
+};
+
+#ENDIF
