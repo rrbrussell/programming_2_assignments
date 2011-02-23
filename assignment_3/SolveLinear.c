da6c202ffa96ecf1bc28b925cdf0fb994d6ec55e
Robert R. Russell <robert@rrbrussell.com>
2011-02-23 10:31:42 -0600

    Main completed, Inital fleshout of SolveLinear and SolveQuadratic

---
+++ assignment_3/SolveLinear.c
@@ -0,0 +1,9 @@
+/* Programming 2 - Assignment 3
+   Robert R. Russell
+   SolveLinear.c
+*/
+
+#include "SolveLinear.h"
+
+void SolveLinear(float b, float c) {
+}
---
+++ assignment_3/SolveQuadratic.h
@@ -0,0 +1,11 @@
+/* Programming 2 - Assignment 3
+   Robert R. Russell
+   SolveQuadratic.h
+*/
+
+#ifndef ASSIGNMENT_3_SOLVEQUADRATIC_H
+#define ASSIGNEMNT_3_SOLVEQUADRATIC_H
+
+void SolveQuadratic(float a, float b, float c);
+
+#endif
