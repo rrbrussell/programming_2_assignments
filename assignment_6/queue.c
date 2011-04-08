a883385b3fb188f61017e4d99d9dcca9c336053e
Robert R. Russell <robert@rrbrussell.com>
2011-04-08 09:29:14 -0500

    A new branch for temporary storage

---
+++ assignment_6/queue.c
@@ -0,0 +1,9 @@
+/* assignment_6/queue.c
+   Queue implementation for assignment 6
+   Requires C99 for bool type
+*/
+
+#include "queue.h"
+#include <stdlib.h>
+#include <stdio.h>
+
---
+++ assignment_6/queue.h
@@ -0,0 +1,34 @@
+/* assignment_6/queue.h
+   Queue prototypes for assignment_6
+   Requires C99
+*/
+
+#ifndef ASSIGNMENT_6_QUEUE_H
+#define ASSIGNMENT_6_QUEUE_H
+
+#include <stdbool.h>
+
+/* Prototype the struct in the typedef just to make the compiler happy */
+typedef struct aNode *queueNode;
+
+struct aNode {
+  int data;
+  queueNode next;
+};
+
+typedef struct queueStruct {
+  queueNode first;
+  queueNode last;
+} *queue;
+
+void initializeQueue(queue aQueue);
+
+int takeFromQueue(queue aQueue);
+void addToQueue(queue aQueue, int data);
+
+bool isQueueEmpty(queue aQueue);
+bool isQueueFull(queue aQueue);
+
+void printQueue(queue aQueue);
+void emptyQueue(queue aQueue);
+#endif
