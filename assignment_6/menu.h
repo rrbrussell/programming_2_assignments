564be2d6d9ce9b5ec7cc856b8ba187ebfa5ce515
Robert R. Russell <robert@rrbrussell.com>
2011-04-17 13:14:31 -0500

    Adding missing inclusion guards on menu.h

--- assignment_6/menu.h
+++ assignment_6/menu.h
@@ -2,6 +2,9 @@
    Menu tools for Programming 2 Assignment 6
 */
 
+#ifndef ASSIGNMENT_6_MENU_H
+#define ASSIGNMENT_6_MENU_H
+
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -15,3 +18,5 @@ char mainMenuString[] =
 char choiceString[] = "Your Choice: ";
 
 int fetchINT(FILE* stream);
+
+#endif
